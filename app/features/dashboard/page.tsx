160f3495599bb36ece777b0323ff9e04c0630627
Nedy King <nedyking007@gmail.com>
2025-09-15 19:58:55 +0100

    Page navOpen solved

--- app/features/dashboard/page.tsx
+++ app/features/dashboard/page.tsx
@@ -74,7 +74,9 @@ export default function DashboardPage() {
   return (
     <div
       className="min-h-screen bg-gray-50 overflow-x-hidden"
-      onClick={() => setNavOpen(!navOpen)}
+      onClick={() => {
+        !navOpen && setNavOpen(!navOpen);
+      }}
     >
       <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
 
